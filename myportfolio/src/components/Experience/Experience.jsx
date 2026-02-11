97a45d0edaa6cbb7f316171024eba5bca7ca80ea
Krishna Yadav <krishanayadav9999@gmail.com>
2026-02-11 15:20:11 +0530

    feat: update About component with TypeAnimation and improved layout spacing

--- myportfolio/src/components/Experience/Experience.jsx
+++ myportfolio/src/components/Experience/Experience.jsx
@@ -1,40 +1,40 @@
 import React from "react";
-import { education } from "../../constants"; // Import the education data
+import { experiences } from "../../constants"; // Import your data
 
-const Education = () => {
+const Experience = () => {
   return (
     <section
-      id="education"
-      className="py-24 pb-24 px-[12vw] md:px-[7vw] lg:px-[16vw] font-sans bg-skills-gradient clip-path-custom-3"
+      id="experience"
+      className="py-24 pb-24 px-[12vw] md:px-[7vw] lg:px-[16vw] font-sans bg-skills-gradient clip-path-custom-2"
     >
       {/* Section Title */}
       <div className="text-center mb-16">
-        <h2 className="text-4xl font-bold text-white">EDUCATION</h2>
+        <h2 className="text-4xl font-bold text-white">EXPERIENCE</h2>
         <div className="w-32 h-1 bg-purple-500 mx-auto mt-4"></div>
         <p className="text-gray-400 mt-4 text-lg font-semibold">
-          My education has been a journey of learning and development. Here are
-          the details of my academic background
+          A collection of my work experience and the roles I have taken in
+          various organizations
         </p>
       </div>
 
-      {/* Education Timeline */}
+      {/* Experience Timeline */}
       <div className="relative">
         {/* Vertical line */}
         <div className="absolute sm:left-1/2 left-0 transform -translate-x-1/2 sm:-translate-x-0 w-1 bg-white h-full"></div>
 
-        {/* Education Entries */}
-        {education.map((edu, index) => (
+        {/* Experience Entries */}
+        {experiences.map((experience, index) => (
           <div
-            key={edu.id}
+            key={experience.id}
             className={`flex flex-col sm:flex-row items-center mb-16 ${
-              index % 2 === 0 ? "sm:justify-start" : "sm:justify-end"
+              index % 2 === 0 ? "sm:justify-end" : "sm:justify-start"
             }`}
           >
             {/* Timeline Circle */}
             <div className="absolute sm:left-1/2 left-0 transform -translate-x-1/2 bg-gray-400 border-4 border-[#8245ec] w-12 h-12 sm:w-16 sm:h-16 rounded-full flex justify-center items-center z-10">
               <img
-                src={edu.img}
-                alt={edu.school}
+                src={experience.img}
+                alt={experience.company}
                 className="w-full h-full object-cover rounded-full"
               />
             </div>
@@ -47,32 +47,46 @@ const Education = () => {
             >
               {/* Flex container for image and text */}
               <div className="flex items-center space-x-6">
-                {/* School Logo/Image */}
-                <div className="w-24 h-16 bg-white rounded-md overflow-hidden">
+                {/* Company Logo/Image */}
+                <div className="w-16 h-16 bg-white rounded-md overflow-hidden">
                   <img
-                    src={edu.img}
-                    alt={edu.school}
+                    src={experience.img}
+                    alt={experience.company}
                     className="w-full h-full object-cover"
                   />
                 </div>
 
-                {/* Degree, School Name, and Date */}
+                {/* Role, Company Name, and Date */}
                 <div className="flex flex-col justify-between">
                   <div>
-                    <h3 className="text-xl sm:text-xl font-semibold text-white">
-                      {edu.degree}
+                    <h3 className="text-xl sm:text-2xl font-semibold text-white">
+                      {experience.role}
                     </h3>
                     <h4 className="text-md sm:text-sm text-gray-300">
-                      {edu.school}
+                      {experience.company}
                     </h4>
                   </div>
                   {/* Date at the bottom */}
-                  <p className="text-sm text-gray-500 mt-2">{edu.date}</p>
+                  <p className="text-sm text-gray-500 mt-2">
+                    {experience.date}
+                  </p>
                 </div>
               </div>
 
-              <p className="mt-4 text-gray-400 font-bold">Grade: {edu.grade}</p>
-              <p className="mt-4 text-gray-400">{edu.desc}</p>
+              <p className="mt-4 text-gray-400">{experience.desc}</p>
+              <div className="mt-4">
+                <h5 className="font-medium text-white">Skills:</h5>
+                <ul className="flex flex-wrap mt-2">
+                  {experience.skills.map((skill, index) => (
+                    <li
+                      key={index}
+                      className="bg-[#8245ec] text-gray-300 px-4 py-1 text-xs sm:text-sm rounded-lg mr-2 mb-2 border border-gray-400"
+                    >
+                      {skill}
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
           </div>
         ))}
@@ -81,4 +95,4 @@ const Education = () => {
   );
 };
 
-export default Education;
+export default Experience;
